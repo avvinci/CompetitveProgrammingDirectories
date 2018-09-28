5b017d5cd423059dafc0d52c70f1bb1febb554e8
abhinav vinci <vinciabhinav7@gmail.com>
2018-09-28 12:10:04 +0530

    Update 111.cpp

--- CodeForces/111.cpp
+++ CodeForces/111.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 typedef pair<long long,long long > P;
 
-#define mod 1000000007 
-#define pb emplace_back
+#define mod 998244353 
+#define pb push_back
 #define fs first
 #define sc second
 #define ll long long 
@@ -14,14 +14,51 @@ typedef pair<long long,long long > P;
 #define all(container) container.begin(),container.end()
 #define INP ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define ws(x) cout << #x << " = " << x << endl;
-#define N 1000004
+#define N 1004
 
-const long long infl = 1e13+5;
+const long long infl = 1e18+5;
 
-ll n,m,k,q,f,val,t,i,j;
+ll n,m,k,q,x,y,f,val,t,i,j;
 ll ind,cnt,sz,sm,ans,mx,mn;
-ll a[N][3] ,b[N][3] , x[N][3] , y[N][3] ;
+ll dp[N][N+N][5];
 
+ll rec(int n ,int k ,int st){
+
+	if(k < 0 ) return 0 ; 
+	if(n == 0){
+		if(k == 1 ){
+			if(st < 1) return 1 ; 
+		}
+		if( k == 2) {
+			if(st > 1) return 1 ; 
+		}
+		return  0; 
+	}
+	 
+	if(dp[n][k][st] != -1 ) return dp[n][k][st] ; 
+
+	ll ans = 0 ; 
+	ans += rec(n-1,k,st) ;   ans %= mod; 
+
+
+	if(st < 2 ){
+		ans += rec(n-1,k-2,1-st) ;  ans %= mod; 
+		ans += rec(n-1,k-1,2) ;  ans %= mod; 
+		ans += rec(n-1,k-1,3) ;  ans %= mod; 
+	}
+	else{
+		ans += rec(n-1,k-1,0) ;  ans %= mod; 
+		ans += rec(n-1,k-1,1) ;  ans %= mod; 
+		if(st == 2)
+			ans += rec (n-1,k-2,3) ;
+		else
+			ans += rec(n-1,k-2,2)  ; 
+
+		 ans %= mod; 
+	}
+	dp[n][k][st] = ans ; 
+	return ans; 
+}
 
 int main(){
 INP
@@ -30,36 +67,30 @@ if (fopen("inp.txt", "r")) {
     freopen("inp.txt", "r", stdin);
 }
 
-cin>>n;
-fr(i,0,3){
-	a[n+1][i] = a[0][i] = b[0][i] = b[n+1][i] = -infl ;
-	x[0][i] = y[0][i] = x[n+1][i] = y[n+1][i] = infl ; 
-}
-fr(i,1,n+1){
-	cin>>a[i][0]>>b[i][0]>>x[i][0]>>y[i][0] ; 
-	a[i][1] = max(a[i][0], a[i-1][1]) ; 
-	b[i][1] = max(b[i][0], b[i-1][1]) ;
-	x[i][1] = min(x[i][0], x[i-1][1]) ;
-	y[i][1] = min(y[i][0], y[i-1][1]) ; 
-	
+cin>>n>>k ; 
+if(n == 1) {
+	cout<<"2" ;
+	return 0 ; 
 }
 
-for(i=n;i>=1 ;i--){
-	a[i][2] = max(a[i][0], a[i+1][2]) ; 
-	b[i][2] = max(b[i][0], b[i+1][2]) ;
-	x[i][2] = min(x[i][0], x[i+1][2]) ;
-	y[i][2] = min(y[i][0], y[i+1][2]) ; 
+memset(dp,-1, sizeof(dp)) ; 
+ans = 0 ; 
+ans += rec(n-1,k,1) ; ans %= mod; 
+ans += rec(n-1,k,2) ; ans %= mod; 
+ans += rec(n-1,k,3) ; ans %= mod; 
+ans += rec(n-1,k,0) ; ans %= mod; 
+cout<<ans ; 
 
-	// cout<<a[i][2] <<" " <<b[i][2]<<" " <<x[i][2]<<" "<<y[i][2]<<"\n" ;
-}
 
-fr(i,1,n+1){
-	// cout<<a[i-1][1] <<" " <<b[i-1][1]<<" " <<x[i-1][1]<<" "<<y[i-1][1]<<"\n" ;  
-	if(max(a[i-1][1] ,a[i+1][2]) <= min(x[i-1][1] ,x[i+1][2]) &&  max(b[i-1][1] ,b[i+1][2]) <= min(y[i-1][1],y[i+1][2]) ){
-		cout<<max(a[i-1][1] ,a[i+1][2])  <<" " <<  max(b[i-1][1] ,b[i+1][2]);
-		return 0 ; 
-	}
-}
+
+
+
+
+
+
+
+
+
 
 
 return 0 ;
