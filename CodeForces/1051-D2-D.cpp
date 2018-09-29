4d6e125b01bca0b638e686c9e01211cecddd71c8
abhinav vinci <vinciabhinav7@gmail.com>
2018-09-29 12:25:52 +0530

    Create 1051-D2-D.cpp

---
+++ CodeForces/1051-D2-D.cpp
@@ -0,0 +1,93 @@
+// avvinci -  summers 18    
+// #include"prettyprint.hpp"
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef pair<long long,long long > P;
+
+#define mod 998244353 
+#define pb push_back
+#define fs first
+#define sc second
+#define ll long long 
+#define fr(it,st,en) for(it=st;it<en;it++)
+#define all(container) container.begin(),container.end()
+#define INP ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ws(x) cout << #x << " = " << x << endl;
+#define N 1004
+
+const long long infl = 1e18+5;
+
+ll n,m,k,q,x,y,f,val,t,i,j;
+ll ind,cnt,sz,sm,ans,mx,mn;
+ll dp[N][N+N][5];
+
+ll rec(int n ,int k ,int st){
+
+	if(k < 0 ) return 0 ; 
+	if(n == 0){
+		if(k == 1 ){
+			if(st < 2) return 1 ; 
+		}
+		if( k == 2) {
+			// cout<<st<<"..\n\n";
+			if(st > 1) return 1 ; 
+		}
+		return  0; 
+	}
+	 
+	if(dp[n][k][st] != -1 ) return dp[n][k][st] ; 
+
+	ll ans = 0 ; 
+	ans += rec(n-1,k,st) ;   ans %= mod; 
+
+
+	if(st < 2 ){
+		ans += rec(n-1,k-1,1-st) ;  ans %= mod; 
+		ans += rec(n-1,k,2) ;  ans %= mod; 
+		ans += rec(n-1,k,3) ;  ans %= mod; 
+	}
+	else{
+		ans += rec(n-1,k-1,0) ;  ans %= mod; 
+		ans += rec(n-1,k-1,1) ;  ans %= mod; 
+		if(st == 2)
+			ans += rec (n-1,k-2,3) ;
+		else
+			ans += rec(n-1,k-2,2)  ; 
+
+		 ans %= mod; 
+	}
+	dp[n][k][st] = ans ; 
+	// cout<<n<<" " << k<<" " <<st <<"  -- " << ans<<"  .\n" ; 
+	return ans; 
+}
+
+int main(){
+INP
+if (fopen("inp.txt", "r")) {
+    freopen("myfile.txt","w",stdout);
+    freopen("inp.txt", "r", stdin);
+}
+
+cin>>n>>k ; 
+memset(dp,-1, sizeof(dp)) ; 
+ll ans = 0 ; 
+ans += rec(n-1,k,2) ; ans %= mod; 
+ans += rec(n-1,k,1) ; ans %= mod;
+ans *= 2LL ; ans %= mod; 
+cout<<ans ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0 ;
+}
